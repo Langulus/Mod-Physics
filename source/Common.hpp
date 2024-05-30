b29e894e58094e3d7e0890922d83e56eb5677571
Dimo Markov <dimomarkov@gmail.com>
2024-05-30 03:20:35 +0300

    Minor improvements

--- source/Common.hpp
+++ source/Common.hpp
@@ -24,5 +24,10 @@ namespace Euclidean
    struct Field;
 }
 
-#define VERBOSE_PHYSICS(...)      Logger::Verbose(Self(), __VA_ARGS__)
-#define VERBOSE_PHYSICS_TAB(...)  const auto tab = Logger::Verbose(Self(), __VA_ARGS__, Logger::Tabs {})
+#if 0
+   #define VERBOSE_PHYSICS(...)      Logger::Verbose(Self(), __VA_ARGS__)
+   #define VERBOSE_PHYSICS_TAB(...)  const auto tab = Logger::Verbose(Self(), __VA_ARGS__, Logger::Tabs {})
+#else
+   #define VERBOSE_PHYSICS(...)      LANGULUS(NOOP)
+   #define VERBOSE_PHYSICS_TAB(...)  LANGULUS(NOOP)
+#endif
